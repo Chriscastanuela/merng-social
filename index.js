9feecfa6c8a5707b3d4ceea2bfbc74fe7ecea6b8
Chriscastanuela <chriscastanuela@ymail.com>
2022-06-17 00:25:15 -0600

    connect to mongoose

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { ApolloServer } = require('apollo-server');
 const gql = require('graphql-tag');
 const mongoose = require('mongoose');
-const { MONGODB } = require('config.js');
+const { MONGODB } = require('./config.js');
 
 const typeDefs = gql`
     type Query{
@@ -20,7 +20,11 @@ const server = new ApolloServer({
     resolvers
 });
 
-server.listen({port: 3000})
+mongoose.connect(MONGODB, {useNewUrlParser: true})
+    .then(() => {
+        console.log('Mongo connected');
+        return server.listen({port: 3000});
+    })
     .then(res => {
         console.log(`Server running at ${res.url}`);
     })
